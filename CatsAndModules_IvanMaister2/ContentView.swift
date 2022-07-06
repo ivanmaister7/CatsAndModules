6be2a2f2fac6786d101d57ba90bd3c1d68f77892
Ivan Maister <ivanmaister7@gmail.com>
2022-07-06 16:20:57 +0300

    Initial Commit

---
+++ CatsAndModules_IvanMaister2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatsAndModules_IvanMaister2
+//
+//  Created by Master on 06.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
